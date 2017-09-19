b420d007b5d1253600fdf807f9b2562fc86c2cf1
steadfasterX <steadfastX@boun.cr>
2017-09-19 18:23:04 +0200

    new: using gen func

--- livepatcher/livepatcher.sh
+++ livepatcher/livepatcher.sh
@@ -33,12 +33,12 @@ LOG=${LOGPATH}/livepatcher.log
 echo -e "\n$(date) - Starting $0" >> $LOG
 
 # include FWUL functions
-if [ -r "$FWULLIB" ];then
-	source $FWULLIB
-	[ $DEBUG -eq 1 ] && echo "sourced $FWULLIB"
+if [ -r "$GENFUNC" ];then
+	source $GENFUNC
+	[ $DEBUG -eq 1 ] && echo "sourced $GENFUNC"
 else
 	F_ERR "cant find needed library file" $LOG
-	F_EXIT "$0 LIB" "3"
+	F_EXIT "$0 GENFUNC" "3"
 fi
 
 # include FWUL release info
